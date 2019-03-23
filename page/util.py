70fb88f93f9106fe3fe6992a824b00b352d6c7d6
Sapphire Becker <github@logicplace.com>
2019-03-23 02:32:27 -0700

    add clean filename util function

--- page/util.py
+++ page/util.py
@@ -1,3 +1,6 @@
+import string
+import unicodedata
+
 import mwparserfromhell
 
 def get_text(code):
@@ -11,6 +14,33 @@ def get_text(code):
 		return get_text(code.text or code.title)
 	if isinstance(code, mwparserfromhell.nodes.tag.Tag):
 		return get_text(code.contents)
+	if isinstance(code, mwparserfromhell.nodes.comment.Comment):
+		return ""
 	if isinstance(code, str):
 		return code
 	raise Exception(f"get_text not implemented for {type(code)}")
+
+
+# https://gist.github.com/wassname/1393c4a57cfcbf03641dbc31886123b8
+valid_filename_chars = f"-.() {string.ascii_letters}{string.digits}"
+
+def clean_filename(filename, whitelist=valid_filename_chars):
+	name, *ext = filename.split(".", 1)
+	ext = ext[0] if ext else None
+
+	# Keep only valid ascii chars.
+	cleaned_filename = unicodedata.normalize("NFKD", name).encode("ASCII", "ignore").decode()
+
+	# Keep only whitelisted chars.
+	cleaned_filename = "".join(c for c in cleaned_filename if c in whitelist)
+
+	# Truncate to Windows max length.
+	target_len = 255
+	if ext is not None:
+		target_len -= len(ext) + 1
+
+	cleaned_filename = cleaned_filename[:target_len]
+
+	if ext is not None:
+		return f"{cleaned_filename}.{ext}"
+	return cleaned_filename
